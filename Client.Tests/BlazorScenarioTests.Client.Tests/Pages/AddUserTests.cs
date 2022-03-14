a890d8aece06c880f90e63cd5d815e0b0fd31497
FilipKostrzewa <101265155+FilipKostrzewa@users.noreply.github.com>
2022-03-15 00:46:01 +0100

    scenario tests for Counter page

---
+++ Client.Tests/BlazorScenarioTests.Client.Tests/Pages/AddUserTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BlazorScenarioTests.Client.Pages.AddUser;
+using BlazorScenarioTests.Client.Pages.AddUser.Reducers;
+using Fluxor;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorScenarioTests.Client.Tests.Pages
+{
+    public partial class AddUserTests
+    {
+        private readonly IServiceProvider ServiceProvider;
+        private readonly IStore Store;
+        private readonly IState<AddUserState> State;
+        public AddUserTests()
+        {
+            var services = new ServiceCollection();
+            services.AddFluxor(x =>
+                x
+                    .ScanAssemblies(GetType().Assembly)
+                    .ScanTypes(typeof(AddUserState), typeof(CopyAddressReducer)));
+
+            ServiceProvider = services.BuildServiceProvider();
+            Store = ServiceProvider.GetRequiredService<IStore>();
+            State = ServiceProvider.GetRequiredService<IState<AddUserState>>();
+            Store.InitializeAsync().Wait();
+        }
+    }
+}
---
+++ Client.Tests/BlazorScenarioTests.Client.Tests/Pages/CounterTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using BlazorScenarioTests.Client.Pages;
+using Bunit;
+using ScenarioTests;
+using Xunit;
+
+namespace BlazorScenarioTests.Client.Tests.Pages
+{
+    public partial class CounterTests
+    {
+        [Scenario]
+        public async Task TripleIncrement(ScenarioContext scenario)
+        {
+            using var context = new TestContext();
+            var page = context.RenderComponent<Counter>();
+
+            page.Instance.IncrementCount();
+            page.Render();
+            scenario.Fact("First increment set currentCount to 1", () =>
+            {
+                Assert.True(page.Instance.currentCount == 1);
+            });
+            scenario.Fact("First increment - page contains proper caption", () =>
+            {
+                var paragraphs = page.FindAll("p");
+                var paragraph = paragraphs[0];
+                Assert.True(paragraph.InnerHtml == "Current count: 1");
+            });
+
+            page.Instance.IncrementCount();
+            page.Render();
+            scenario.Fact("Second increment set currentCount to 2", () =>
+            {
+                Assert.True(page.Instance.currentCount == 2);
+            });
+            scenario.Fact("Second increment - page contains proper caption", () =>
+            {
+                var paragraphs = page.FindAll("p");
+                var paragraph = paragraphs[0];
+                Assert.True(paragraph.InnerHtml == "Current count: 2");
+            });
+
+            page.Instance.IncrementCount();
+            page.Render();
+            scenario.Fact("Third increment set currentCount to 3", () =>
+            {
+                Assert.True(page.Instance.currentCount == 3);
+            });
+            scenario.Fact("Third increment - page contains proper caption", () =>
+            {
+                var paragraphs = page.FindAll("p");
+                var paragraph = paragraphs[0];
+                Assert.True(paragraph.InnerHtml == "Current count: 3");
+            });
+
+        }
+    }
+}
